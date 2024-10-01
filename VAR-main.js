abb80c55045ba5480c6a529307f655714292f464
Geraxi <umbertogeraci0@gmail.com>
2024-10-01 23:31:27 +0200

    Create VAR-main.js

---
+++ VAR-main.js
@@ -0,0 +1,17 @@
+let num1= 5
+let num2= 10
+
+console.log("il valore della variabile num1 è:"+ num1);
+console.log("il valore della variabile num2 è:"+ num2);
+
+let stringa= 'Buona sera a tutti';
+
+stringa='Benvenuti';
+console.log('il valore della variabile stringa è ' +stringa);
+
+const pGreco= 3.14;
+
+pGreco=100;
+console.log('il valore di pGreco é ' + pGreco);
+
+// dà errore perchè una costante non puo essere sostituita
