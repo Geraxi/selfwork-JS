33004fff0e16c776b137ae0bcc2830e1fb2a2571
Geraxi <umbertogeraci0@gmail.com>
2024-10-21 16:54:29 +0200

    Update sw-dom2.js

--- sw-dom2.js
+++ sw-dom2.js
@@ -1,24 +1,25 @@
+let input = document.querySelector('input');
+let text = document.querySelector('textarea');
+let btn = document.querySelector('button');
+let container = document.querySelector('.container');
 
-let input= document.querySelector('input')
-let text= document.querySelector('textarea')
-let btn= document.querySelector('button')
-let container= document.querySelector('.container')
+btn.addEventListener('click', () => {
+    let date = new Date();
+    let formatDate = date.toLocaleDateString(); // Correzione qui
 
+    if (input.value === '' || text.value === '') { // Correzione qui
+        alert('I campi sono vuoti, non puoi creare il tuo articolo');
+    } else {
+        let div = document.createElement('div');
+        div.style.backgroundColor = '#d3d3d3';
+        div.style.padding = '20px';
+        div.style.marginTop = '20px';
+        div.style.border = '2px solid black';
+        div.innerHTML = `<h2>${input.value}</h2> <p>${text.value}</p> <i>${formatDate}</i>`;
+        container.appendChild(div);
 
-btn.addEventListener('click',()=>{
-    let date= new Date()
-    let formatDate= date.tolacaleDateString
-    if(input.value=='' && text.value==''){
-        alert('i campi sono vuoti non puoi creare il tuo articolo')
-    }else{
-        let div= document.createElement('div')
-        div.style.backgroundColor='#d3d3d3'
-        div.style.padding='20px'
-        div.style.marginTop='20px'
-        div.style.border= '2px solid black'
-        div.innerHTML=`<h2>${input.value}</h2> <p>${text.value}</p> <i>${formatDate}</i>`
-        container.appendChild(div)
-        input.value=''
-        text.value=''
+        // Resetta i campi input e textarea
+        input.value = '';
+        text.value = '';
     }
-})
+});
