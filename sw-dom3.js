acff244ca2ce0770751761d3cb17accbd9709b99
Geraxi <umbertogeraci0@gmail.com>
2024-10-28 19:50:34 +0100

    Update sw-dom3.js

--- sw-dom3.js
+++ sw-dom3.js
@@ -9,32 +9,41 @@ let counter;
 let remainingSeconds = 0;
 
 startBtn.addEventListener('click', () => {
-    clearInterval(interval); // Cancella intervallo precedente
-    if (countdownInput.value) {
-        counter = countdownInput.value;
+    clearInterval(interval); // Cancella l'intervallo precedente
+
+    // Inizializza counter e remainingSeconds con il valore inserito o precedente
+    if (countdownInput.value && parseInt(countdownInput.value, 10) > 0) {
+        counter = parseInt(countdownInput.value, 10);
+        remainingSeconds = counter;
     } else if (remainingSeconds !== 0) {
-        counter = remainingSeconds; // continua da dove si é fermato
+        counter = remainingSeconds;
+    } else {
+        alert('Inserisci un valore di countdown valido!');
+        return;
     }
-    
+
     interval = setInterval(() => {
-        if (counter <= 0) {
+        if (counter < 0) {
             clearInterval(interval);
             remainingTime.innerHTML = 'Tempo scaduto'; // Countdown finito
         } else {
-            remainingTime.innerHTML = counter; // Update al display 
+            remainingTime.innerHTML = counter; // Aggiorna il display
             counter--;
+            remainingSeconds = counter; // Aggiorna remainingSeconds
         }
     }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
-    clearInterval(interval); // ferma l'intervallo
-    remainingSeconds = counter; //  salva il counter attuale
+    clearInterval(interval); // Ferma l'intervallo
+    remainingSeconds = counter; // Salva il valore attuale
 });
 
 resetBtn.addEventListener('click', () => {
     countdownInput.value = ''; // Cancella input
-    clearInterval(interval); // Stop all'intervallo
-    remainingTime.innerHTML = ''; // Reset display
-    remainingSeconds = 0; //resetta lorario salvato
+    clearInterval(interval); // Ferma l'intervallo
+    remainingTime.innerHTML = '00:00'; // Reset del display
+    remainingSeconds = 0; // Resetta l'orario salvato
 });
+
+
