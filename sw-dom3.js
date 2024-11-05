a7a6bd8479966091f1c2e8481b6ed894dedfaa59
Geraxi <umbertogeraci0@gmail.com>
2024-11-05 20:48:28 +0100

    Update sw-dom3.js

--- sw-dom3.js
+++ sw-dom3.js
@@ -1,49 +1,42 @@
-let remainingTime = document.querySelector('#remainingTime');
-let countdownInput = document.querySelector('#countdown-input');
-let startBtn = document.querySelector('#startBtn');
-let stopBtn = document.querySelector('#stopBtn');
-let resetBtn = document.querySelector('#resetBtn');
+let remainingTime= document.querySelector('#remainingTime')
+let countdownInput= document.querySelector('#countdown-input')
+let startBtn= document.querySelector('#startBtn')
+let stopBtn= document.querySelector('#stopBtn')
+let resetBtn= document.querySelector('#resetBtn')
 
 let interval;
 let counter;
-let remainingSeconds = 0;
+let remainingSeconds=0
 
-startBtn.addEventListener('click', () => {
-    clearInterval(interval); // Cancella l'intervallo precedente
+startBtn.addEventListener('click', ()=>{
+    clearInterval(interval)
+    counter=countdownInput.value
 
-    // Inizializza counter e remainingSeconds con il valore inserito o precedente
-    if (countdownInput.value && parseInt(countdownInput.value, 10) > 0) {
-        counter = parseInt(countdownInput.value, 10);
-        remainingSeconds = counter;
-    } else if (remainingSeconds !== 0) {
-        counter = remainingSeconds;
-    } else {
-        alert('Inserisci un valore di countdown valido!');
-        return;
+    if (remainingSeconds !==0){
+        counter= remainingSeconds;
     }
 
-    interval = setInterval(() => {
-        if (counter < 0) {
-            clearInterval(interval);
-            remainingTime.innerHTML = 'Tempo scaduto'; // Countdown finito
-        } else {
-            remainingTime.innerHTML = counter; // Aggiorna il display
-            counter--;
-            remainingSeconds = counter; // Aggiorna remainingSeconds
+    interval= setInterval (()=>{
+        if(counter <0){
+            clearInterval(interval)
+            remainingTime.innerHTML='tempo scaduto'
+        }else{
+            remainingTime.innerHTML= counter
+            counter --
         }
-    }, 1000);
-});
+    
+    },1000);
+})
 
-stopBtn.addEventListener('click', () => {
-    clearInterval(interval); // Ferma l'intervallo
-    remainingSeconds = counter; // Salva il valore attuale
-});
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(interval)
+    remainingSeconds= counter
+})
 
-resetBtn.addEventListener('click', () => {
-    countdownInput.value = ''; // Cancella input
-    clearInterval(interval); // Ferma l'intervallo
-    remainingTime.innerHTML = '00:00'; // Reset del display
-    remainingSeconds = 0; // Resetta l'orario salvato
-});
+resetBtn.addEventListener('click',()=>{
+    countdownInput.value=''
+    clearInterval(interval)
+    remainingTime.innerHTML=''
+})
 
 
